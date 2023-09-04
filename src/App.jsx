18bba2223f26d014c89c965b9ea9ae9d8075e6cb
unknown <antonyvidetta1986@gmail.com>
2023-09-04 16:30:25 +1200

    Add css framer motion page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import WelcomePage from './pages/Welcome.jsx';
 import ChallengesPage from './pages/Challenges.jsx';
+import FramerPage from './pages/FramerPage.jsx';
 
 const router = createBrowserRouter([
   { path: '/', element: <WelcomePage /> },
   { path: '/challenges', element: <ChallengesPage /> },
+  { path: '/framer', element: <FramerPage />}
 ]);
 
 function App() {
@@ -13,3 +15,4 @@ function App() {
 }
 
 export default App;
+ 
